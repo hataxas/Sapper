e02a75a42d89ef9edbeca3fda9f443db024279bc
Natalia Strumilenko <hataxas@gmail.com>
2021-03-19 17:44:54 +0100

    game win added

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 let gameOver = false;
+// let cell = {
+//   open: false,
+//   closed: true,
+//   flag: false,
+//   bomb: "",
+//   number: '',
+//   neighbours: []
+// };
 
 const board = document.querySelector('.board');
 const element = document.getElementById('cells');
@@ -11,24 +19,12 @@ menuSmallFieldSize.addEventListener("click", smallField);
 menuMediumlFieldSize.addEventListener("click", mediumField);
 menuBigFieldSize.addEventListener("click", bigField);
 
-
-// create cells
-function addCells(numberOfCells) {
-  element.innerHTML = '';
-  gameOver = false;
-  messageDisplay.classList.remove('gameOver');
-  for (let i = 0; i < numberOfCells; i++) {
-    const newCell = document.createElement('div');
-    newCell.classList.add('cell');
-    element.appendChild(newCell);
-  }
-}
-
+//! Draw a field
 // Рисуем поле 10*10
 function smallField() {
   board.classList.add('smallBoard');
   element.classList.add('smallCells');
-  addCells(100);
+  drawCells(100);
   const cells = document.querySelectorAll('.cell');
   for (let i = 0; i < 100; i++) {
     cells[i].classList.add('small');
@@ -41,7 +37,7 @@ function smallField() {
 function mediumField() {
   board.classList.add('mediumBoard');
   element.classList.add('mediumCells');
-  addCells(17 * 17);
+  drawCells(17 * 17);
   const cells = document.querySelectorAll('.cell');
   for (let i = 0; i < (17 * 17); i++) {
     cells[i].classList.add('medium');
@@ -54,7 +50,7 @@ function mediumField() {
 function bigField() {
   board.classList.add('bigBoard');
   element.classList.add('bigCells');
-  addCells(25 * 25);
+  drawCells(25 * 25);
   const cells = document.querySelectorAll('.cell');
   for (let i = 0; i < (25 * 25); i++) {
     cells[i].classList.add('big');
@@ -63,46 +59,55 @@ function bigField() {
   messageDisplay.textContent = "100 flags left";
 }
 
-// Create a field
-function createField(numberOfRows, numberOfColumns, numberOfBombs) {
-  let field = plantBombs(numberOfRows, numberOfColumns, numberOfBombs);
-  shuffle(field);
-  plantNumbers(field, numberOfRows, numberOfColumns);
-  return field;
+// Draw cells
+function drawCells(numberOfCells) {
+  element.innerHTML = '';
+  gameOver = false;
+  messageDisplay.classList.remove('gameOver');
+  for (let i = 0; i < numberOfCells; i++) {
+    const newCell = document.createElement('div');
+    newCell.classList.add('cell');
+    element.appendChild(newCell);
+  }
 }
 
-// Draw a field
+// Draw field
 function drawField(numberOfRows, numberOfColumns, numberOfBombs) {
   const cells = document.querySelectorAll('.cell');
   let field = createField(numberOfRows, numberOfColumns, numberOfBombs);
   let numberOfPlantedFlags = 0;
   for (let i = 0; i < field.length; i++) {
-    cells[i].classList.remove('num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'bomb', 'clicked', 'flag');
+    cells[i].classList.remove('num0', 'num1', 'num2', 'num3', 'num4', 'num5', 'num6', 'num7', 'num8', 'bombs', 'clicked', 'flag');
     cells[i].addEventListener('contextmenu', function (event) {
       event.preventDefault();
       if (!gameOver) {
-        if (!this.classList.contains('flag')) {
+        if (!field[i].flag && !field[i].open) {
           this.classList.add('flag');
+          field[i].flag = true;
+          console.log(field[i]);
           numberOfPlantedFlags++;
-          messageDisplay.textContent = (numberOfBombs - numberOfPlantedFlags) + " " + "flags left";
         } else {
           this.classList.remove('flag');
+          field[i].flag = false;
+          console.log(field[i]);
           numberOfPlantedFlags--;
-          messageDisplay.textContent = (numberOfBombs - numberOfPlantedFlags) + " " + "flags left";
         }
+        messageDisplay.textContent = (numberOfBombs - numberOfPlantedFlags) + " " + "flags left";
       }
     });
-    if (field[i] === '*') {
+    if (field[i].bomb === '*') {
       cells[i].addEventListener('click', function () {
         if (!gameOver) {
           openAllCells(field);
         }
       });
     } else {
-      if ([0, 1, 2, 3, 4, 5, 6, 7, 8].includes(field[i])) {
+      if ([0, 1, 2, 3, 4, 5, 6, 7, 8].includes(field[i].number)) {
         cells[i].addEventListener('click', function () {
           if (!gameOver) {
-            this.classList.add('num' + field[i], 'clicked');
+            this.classList.add('num' + field[i].number, 'clicked');
+            field[i].open = true;
+            checkField(field);
           }
         });
       }
@@ -110,26 +115,62 @@ function drawField(numberOfRows, numberOfColumns, numberOfBombs) {
   }
 }
 
+function checkField(arr) {
+  gameOver = true;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].flag && arr[i].bomb !== '*') {
+      gameOver = false;
+      break;
+    }
+    if (!arr[i].open && !arr[i].flag) {
+      gameOver = false;
+      break;
+    }
+  }
+  if (gameOver) {
+    win(arr);
+  }
+}
+
+//! Create a field
+function createField(numberOfRows, numberOfColumns, numberOfBombs) {
+  let field = plantBombs(numberOfRows, numberOfColumns, numberOfBombs);
+  shuffle(field);
+  plantNumbers(field, numberOfRows, numberOfColumns);
+  return field;
+}
+
+// add cells on the field
+function addCells(numberOfRows, numberOfColumns) {
+  let fieldWithCells = [];
+  let numberOfCells = numberOfRows * numberOfColumns;
+  for (let i = 0; i < numberOfCells; i++) {
+    let cell = {
+      open: false,
+      flag: false,
+      bomb: "",
+      number: '',
+      neighbours: []
+    };
+    fieldWithCells.push(cell);
+  }
+  return fieldWithCells;
+}
 
 //  planting bomb on the field
 function plantBombs(numberOfRows, numberOfColumns, numberOfBombs) {
-  let fieldWithBomb = [];
+  let fieldWithCells = addCells(numberOfRows, numberOfColumns);
   let numberOfPlantingBombs = 0;
-  let numberOfFilledCells = 0;
-  let numberOfCells = numberOfRows * numberOfColumns;
-  for (let i = 0; i < numberOfCells; i++) {
-    if (numberOfFilledCells < numberOfCells) {
-      if (numberOfPlantingBombs < numberOfBombs) {
-        fieldWithBomb.push('*');
-        numberOfPlantingBombs++;
-        numberOfFilledCells++;
-      } else {
-        fieldWithBomb.push(' ');
-        numberOfFilledCells++;
-      }
+  let fieldWithBomb = fieldWithCells.map(function (cell) {
+    if (numberOfPlantingBombs < numberOfBombs) {
+      cell['bomb'] = '*';  //!
+      numberOfPlantingBombs++;
+      return cell;
+    } else {
+      cell['bomb'] = ' ';  //!
+      return cell;
     }
-  }
-  //console.log(fieldWithBomb);
+  });
   return fieldWithBomb;
 }
 
@@ -163,7 +204,7 @@ function coordsToIndex(k, numberOfColumns) {
 function findNeighbours(numberOfRows, numberOfColumns, k) {
   let x = k[0];
   let y = k[1];
-  let neighbours = [];
+  let neighbours = []; //!
   if (x !== 0 && y !== 0 && x !== (numberOfRows - 1) && y !== (numberOfColumns - 1)) {
     neighbours = [[x, y - 1], [x, y + 1], [x - 1, y], [x + 1, y], [x - 1, y - 1], [x - 1, y + 1], [x + 1, y - 1], [x + 1, y + 1]];
   }
@@ -199,34 +240,44 @@ function plantNumbers(arr, numberOfRows, numberOfColumns) {
   for (let i = 0; i < arr.length; i++) {
     let k = indexToCoords(i, numberOfColumns);
     let neighbours = findNeighbours(numberOfRows, numberOfColumns, k);
-    if (arr[i] !== '*') {
+    arr[i].neighbours = neighbours; //!
+    if (arr[i].bomb !== '*') { //!
       let num = 0;
       for (let j = 0; j < neighbours.length; j++) {
         let neighbourI = coordsToIndex(neighbours[j], numberOfColumns)
-        if (arr[neighbourI] === '*') {
+        if (arr[neighbourI].bomb === '*') {
           num++;
         }
       }
-      arr[i] = num;
+      arr[i].number = num; //!
     } else {
-      arr[i] = '*';
+      arr[i].number = ''; //!
     }
   }
   return arr;
 }
 
+//! Game over
 function openAllCells(arr) {
   const cells = document.querySelectorAll('.cell');
   gameOver = true;
   messageDisplay.textContent = "Game over!!!";
   messageDisplay.classList.add('gameOver');
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === '*') {
-      cells[i].classList.add('bomb');
+    arr[i].open = true;
+    if (arr[i].bomb === '*') {
+      cells[i].classList.add('bombs');
     } else {
-      if ([0, 1, 2, 3, 4, 5, 6, 7, 8].includes(arr[i])) {
-        cells[i].classList.add('num' + arr[i], 'clicked');
+      if ([0, 1, 2, 3, 4, 5, 6, 7, 8].includes(arr[i].number)) {
+        cells[i].classList.add('num' + arr[i].number, 'clicked');
       }
     }
   }
+}
+
+function win(arr) {
+  const cells = document.querySelectorAll('.cell');
+  gameOver = true;
+  messageDisplay.textContent = "You won!!!";
+  messageDisplay.classList.add('win');
 }
